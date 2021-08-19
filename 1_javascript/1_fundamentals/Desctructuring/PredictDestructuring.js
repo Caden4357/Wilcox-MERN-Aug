85e09ac40746deba2489250f9b91640840642ec8
Caden Wilcox <wilcox4357@gmail.com>
2021-08-19 16:54:59 -0700

    first jsx

---
+++ 1_javascript/1_fundamentals/Desctructuring/PredictDestructuring.js
@@ -0,0 +1,65 @@
+//This code will output Telsa And Mercedes 
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+
+//This code will output an error because name isnt defined if we comment out line 17 its runs and prints Elon
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+// console.log(name);
+console.log(otherName);
+
+//This code will output another error because theres no password in the person object 
+// it actually ran the code but printed 12345 then undefined 
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+
+//this will output false and then true first=2, second=5, third=2
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+console.log(first);
+
+//this will output value then the whole array then 1 then 5 
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+
+
+function smallest( sortedArray ) {
+    sortedArray = [2, 5, 6, 12, 14, 28, 37, 41, 44, 45]
+        return sortedArray[4];
+    
+    }
+
+console.log(smallest());
+
---
+++ 1_javascript/Algos/BasicAlgos.js
@@ -0,0 +1,198 @@
+function print255(){
+var arr = [];
+    for (var i = 1; i <= 255; i++){
+        arr.push(i);
+}
+    return arr;
+}
+console.log(print255());
+
+const oddNumbers = () => {
+    for (var i = 1; i <= 100; i++){
+        if(i % 2 !== 0){
+            console.log(i);
+        }
+    }
+}
+oddNumbers();
+
+const sumOddNumbers = () => {
+    let sum = 0;
+    for (var i = 1; i <= 5000; i++){
+        if(i % 2 !== 0){
+            sum += i;
+        }
+    }
+    return sum;
+}
+console.log(sumOddNumbers());
+
+const itterateArray = (arr) => {
+    for(var i = 0; i < arr.length; i++){
+        console.log(arr[i]);
+    }
+}
+itterateArray([3,5,6,7,8,9]);
+
+const findMax = (arr) => {
+    let max = arr[0];
+    for (var i = 0; i < arr.length; i++){
+        if (arr[i] > max){
+            max = arr[i];
+        }
+    }
+    return max;
+}
+console.log(findMax([3,8,99,10,100,890,20200203993434, 1,-300,-900,-10000, 9000292,2020020399346]));
+
+const findAverage = (arr) => {
+    let sum = 0;
+    for (var i = 0; i < arr.length; i++){
+        sum += arr[i];
+    }
+    let avg = sum / arr.length;
+    return "the average of this array is " + avg;
+}
+console.log(findAverage([2,4,6,8,10]));
+
+const arrayWithOddNumbers = () => {
+    let y = [];
+    for (var i = 1; i <= 255; i++){
+        if (i % 2 !== 0){
+            y.push(i);
+        }
+    }
+    return y;
+}
+console.log(arrayWithOddNumbers());
+
+const greaterThanY = (arr, y) => {
+    let count = 0;
+    for (var i = 0; i < arr.length; i++){
+        if (arr[i] > y){
+            count ++;
+        }
+    }
+    return count;
+}
+console.log(greaterThanY([1000,3000,500,7, 200, 6, -29, 1000, 10002, 90000], 1000));
+
+const squareValues = (arr) => {
+    for (var i = 0; i < arr.length; i++){
+        arr[i] = arr[i] * arr[i];
+    }
+    return arr;
+}
+console.log(squareValues([1,5,10,-2]));
+
+const noNegatives = (arr) => {
+    for (var i = 0; i < arr.length; i++){
+        if (arr[i] < 0){
+            arr[i] = 0;
+        }
+    }
+    return arr;
+}
+console.log(noNegatives([1,5,10,-2]));
+
+const minMaxAvg = (arr) => {
+    let min = arr[0];
+    let max = arr[0];
+    let sum = 0;
+    for (var i = 0; i < arr.length; i++){
+        if (arr[i] < min){
+            min = arr[i];
+        }
+        else if(arr[i] > max){
+            max = arr[i];
+        }
+        sum += arr[i];
+    }
+    let avg = sum / arr.length;
+    return "min is: " + min + " max is: " + max + " avg is: " + avg 
+}
+console.log(minMaxAvg([1,5,10,-2]));
+
+const shiftingValues = (arr) => {
+    for (var i = 0; i < arr.length; i++){
+        arr[i] = arr[i+1] ;  
+    }
+    arr[arr.length-1] = 0;
+    return arr;
+}
+console.log(shiftingValues([1,5,10,7,-2]));
+
+const numberToString = (arr) => {
+    for (var i = 0; i < arr.length; i++){
+        if (arr[i] < 0){
+            arr[i] = "Dojo";
+        }
+    }
+    return arr;
+}
+console.log(numberToString([-1,-3,2,-6,66]))
+
+const randomArray = () => {
+    let arr = [];
+    for (var i = 0; i <= 10; i ++){
+        arr.push((Math.floor) (Math.random() * 100))
+    }
+    return arr
+}
+console.log(randomArray());
+
+const swappingValues = (arr) => {
+    let firstValue = arr[0];
+    let secondValue = arr[arr.length-1];
+    arr[0] = secondValue;
+    arr[arr.length-1] = firstValue;
+    return arr;
+}
+console.log(swappingValues([32,2,3,5,7,6.1,3,66,10,-202,-20202,-332]));
+
+const reverse = (arr) => {
+    for (var i = 0; i < Math.floor(arr.length/2); i++){
+        let temp = arr[i];
+        arr[i] = arr[arr.length - 1 - i];
+        arr[arr.length - 1 - i] = temp;
+    }
+    return arr
+}
+console.log(reverse([-3,5,1,3,2,4,10]));
+
+const insertXintoY = (arr, x, y) => {
+    arr[y] = x;
+    return arr;
+}
+console.log(insertXintoY([1, 3, 5, 7], 10, 2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Deck {
+//     constructor() {
+//         const suits = ['Diamond', 'Heart', 'Spade', 'Club'];
+//         const faces = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
+//         const deck = [];
+//         for (const suit of suits) {
+//             for (const face of faces) {
+//             deck.push(this.createCard(suit, face));
+//             }
+//         }
+//         this.deck = deck;
+//         }
+//         createCard(suit, face) {
+//         return face + " of " + suit;
+//         }
+// }
+// deck1 = new Deck;
+// console.log(deck1);
