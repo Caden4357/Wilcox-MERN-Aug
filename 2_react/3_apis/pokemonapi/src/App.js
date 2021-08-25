2ea8b9d2d227d863021732ad9b6faafa190538c5
Caden Wilcox <wilcox4357@gmail.com>
2021-08-24 18:56:10 -0700

    axios pokemon

--- 2_react/3_apis/pokemonapi/src/App.js
+++ 2_react/3_apis/pokemonapi/src/App.js
@@ -1,28 +1,39 @@
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
   const [pokemon, setPokemon] = useState([]);
   
+  // useEffect(() => {
+  //   fetch(`https://pokeapi.co/api/v2/pokemon?limit=807`)
+  //     .then(res => {
+  //       return res.json();
+  //     })
+  //     .then(res => {
+  //       setPokemon(res.results);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     })
+  //   }, []);
+
   useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/pokemon?limit=807`)
-      .then(res => {
-        return res.json();
+    axios.get(`https://pokeapi.co/api/v2/pokemon?limit=807`)
+      .then((res) => {
+        console.log(res);
+        setPokemon(res.data.results);
       })
-      .then(res => {
-        setPokemon(res.results);
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-    }, []);
+      .catch((err) => console.log(err))
+  }, []);
   
   return (
     <div className="App">
       <ul>
         {
           pokemon.map((poke, index) => {
-            return (<li key={index}>{poke.name}</li>)
+            return (
+            <li key={index}>{poke.name}</li>)
           })
         }
       </ul>
