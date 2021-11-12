2399a1d665e35df366c8304a1b2b22aa1bc1c290
Caden Wilcox <wilcox4357@gmail.com>
2021-11-12 00:10:59 -0800

    working on image upload for user

--- 5_full_stack_mern/2_advanced_mern/social-media/server/models/user.model.js
+++ 5_full_stack_mern/2_advanced_mern/social-media/server/models/user.model.js
@@ -15,6 +15,11 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: [true, "username is required"]
     },
+    img:
+    {
+        data: Buffer,
+        contentType: String
+    },
     email:{
         type: String,
         required: [true, "username is required"]
--- 5_full_stack_mern/2_advanced_mern/social-media/server/server.js
+++ 5_full_stack_mern/2_advanced_mern/social-media/server/server.js
@@ -1,8 +1,23 @@
 require('dotenv').config();
 const express = require("express");
 const cors = require('cors');
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 const app = express();
+var fs = require('fs');
+var path = require('path');
+var multer = require('multer');
+
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+});
+
+var upload = multer({ storage: storage });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +27,8 @@ app.use(cors({
 }))
 
 app.use(cookieParser());
+// Set EJS as templating engine 
+app.set("view engine", "ejs");
 
 require("./config/mongoose.config");
 
